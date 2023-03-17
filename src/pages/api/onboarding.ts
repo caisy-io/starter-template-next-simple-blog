1e57e9eb286285260272258f93fbd0afa09b67b7
Moritz Gruber <m.gruber@cyclus.digital>
2023-03-17 08:51:53 +0100

    fix: toast 🐛
    fix: toast 🐛

--- src/pages/api/onboarding.ts
+++ src/pages/api/onboarding.ts
@@ -9,7 +9,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.redirect(
-    `https://app.caisy.io/app/project/home?project_id=${process.env.CAISY_PROJECT_ID}?verify_onboarding=true`
+  return res.redirect(
+    `https://app.caisy.io/app/project/home?project_id=${process.env.CAISY_PROJECT_ID}?verify_template_setup=true`
   );
 }
