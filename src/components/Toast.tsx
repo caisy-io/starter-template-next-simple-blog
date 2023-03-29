c519f3851e301fe5cf2428988e180c2d6655f200
Moritz Gruber <m.gruber@cyclus.digital>
2023-03-29 14:06:53 +0200

    chore: update the toast
    chore: update the toast

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -3,14 +3,16 @@ import Link from "next/link";
 export const Toast = () => {
   return (
     <div
-      className="w-100 bg-gray-800 text-sm text-white shadow-lg"
+      className="w-100 bg-gray-800 text-xl lg:text-4xl 2xl:text-6xl text-white shadow-lg"
       role="alert"
     >
-      <div className="p-4 w-100 flex items-center justify-center">
+      <div className="p-4 lg:p-10 2xl:p-40 w-100 flex items-center justify-center">
         <p className="text-center w-100">
           Great Job! You have successfully run the caisy template. Click
           on&nbsp;
           <Link
+            target="_blank"
+            rel="noopener noreferrer"
             href="/api/onboarding"
             className="font-semibold underline text-green-400"
           >
