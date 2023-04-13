9b658cfbb9b1ebecf8c96d48c4505c9d462a4cbb
Moritz Gruber <m.gruber@cyclus.digital>
2023-04-13 08:16:30 +0200

    chore: update @caisy/rich-text-react-renderer and fix type issue
    chore: update @caisy/rich-text-react-renderer and fix type issue

--- src/components/fulltext/FullText.tsx
+++ src/components/fulltext/FullText.tsx
@@ -1,6 +1,4 @@
 import { RichTextRenderer } from "@caisy/rich-text-react-renderer";
-import { FC } from "react";
-import { Asset } from "../Asset";
 import { CenterContainer } from "../CenterContainer";
 import { DocumentLink } from "./overwrites/DocumentLink";
 
@@ -16,7 +14,6 @@ export const FullText: React.FC<IFullText> = ({ text }) => {
           <RichTextRenderer
             node={text?.json}
             overwrites={{
-              // @ts-ignore
               documentLink: (props) =>
                 props?.node && text?.connections ? (
                   <DocumentLink
