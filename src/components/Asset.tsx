6267ccfcb345dbd0b8d16c6a532582689056cda9
Moritz Gruber <m.gruber@cyclus.digital>
2023-04-24 19:58:02 +0200

    chore: added example with unpic image component
    chore: added example with unpic image component

--- src/components/Asset.tsx
+++ src/components/Asset.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { IGenAsset } from "../services/graphql/__generated/sdk";
+import { Image } from "@unpic/react";
 
 interface IAsset extends IGenAsset {
   children?: React.ReactNode;
@@ -10,12 +11,12 @@ export const Asset: React.FC<IAsset> = ({ src, description, children }) => {
     <>
       {src && (
         <div className="flex items-center justify-center overflow-hidden flex-wrap mt-6 mb-6 not-prose">
-          <img
-            loading="lazy"
-            src={`${src}?w=1920&h=960`}
-            srcSet={`${src}?w=3840&h=1920 1920w, ${src}?w=1920&h=960 1280w, ${src}?w=1280&h=640 640w`}
+          <Image
+            src={src}
+            cdn="bunny"
+            layout="fullWidth"
+            className="rounded-lg"
             alt={description ?? ""}
-            className="rounded-lg "
           />
           {description && (
             <label className="text-gray-400 mt-3 text-center block text-xs">
