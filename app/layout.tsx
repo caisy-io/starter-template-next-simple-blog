1bc0f25e36806ec504305fa265d15ecb454c712f
Moritz Gruber <m.gruber@cyclus.digital>
2024-09-24 15:53:34 +0200

    chore: add toast
    chore: add toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import { Navigation } from "../src/layouts/Navigation";
 import { Footer } from "../src/layouts/Footer";
 import { EPageType, getProps } from "../src/services/content/getProps";
+import { Toast } from "../src/components/Toast";
 
 export default async function RootLayout({
   children,
@@ -13,6 +14,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
+        {process.env.NEXT_PUBLIC_SHOW_ONBOARDING_TOAST != "false" && <Toast />}
         <Navigation {...pageProps?.Navigation} />
         {children}
         <Footer {...pageProps?.Footer} />
